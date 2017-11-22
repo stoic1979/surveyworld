a87b92a49004a4041d778638bac3bc26ac84696a
Navjot Singh <weavebytes@gmail.com>
2017-11-22 20:34:11 +0530

    added mlab db

--- db.py
+++ db.py
@@ -19,12 +19,11 @@ class Mdb:
 
     def __init__(self):
         # local db
-        conn_str = "mongodb://%s:%s@%s:%d/%s" \
-             % (DB_USER, DB_PASS, DB_HOST, DB_PORT, AUTH_DB_NAME)
+        # conn_str = "mongodb://%s:%s@%s:%d/%s" \
+        #      % (DB_USER, DB_PASS, DB_HOST, DB_PORT, AUTH_DB_NAME)
 
         # mlab db
-        # conn_str = "mongodb://appdbuser1:" \
-        #             "appdbuser1@ds157712.mlab.com:57712/heroku_188g0kct"
+        conn_str = "mongodb://surveyworlduser:surveyworldpass@ds115436.mlab.com:15436/surveyworld"
         client = MongoClient(conn_str)
         # self.db = client['heroku_188g0kct']
         self.db = client['survey_world'] # local db
